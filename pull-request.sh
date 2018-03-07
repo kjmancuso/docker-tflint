d9b279f793be0bb692dad434976843c68f63c977
Kevin Mancuso <kmancuso@salesforce.com>
2018-03-07 15:27:00 -0600

    giggles

--- pull-request.sh
+++ pull-request.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
 terraform -version
 
 for dir in $(dirname $(find . -path ./.terraform -prune -o -type f -name "*.tf"|sort)|uniq); do
-    echo "Running terraform get in \${dir}..."
-    terraform get \${dir}
-    echo "Running terraform validate in \${dir}..."
-    terraform validate -check-variables=false \$dir
-    echo "Running terraform fmt check in \${dir}..."
-    terraform fmt -check=true -list=true -diff=true -write=false \$dir
+    echo "Running terraform get in ${dir}..."
+    terraform get ${dir}
+    echo "Running terraform validate in ${dir}..."
+    terraform validate -check-variables=false $dir
+    echo "Running terraform fmt check in ${dir}..."
+    terraform fmt -check=true -list=true -diff=true -write=false $dir
 done
 
 echo "Running tflint..."
